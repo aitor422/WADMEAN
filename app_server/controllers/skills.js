1a65b06af712d02b1d17e8fe847bba3882efe0aa
Aitor <chorda.106011@e.unavarra.es>
2018-04-04 17:19:37 +0300

    Translation now happens on the controller rather than the layout file

--- app_server/controllers/skills.js
+++ app_server/controllers/skills.js
@@ -38,6 +38,7 @@ const skills = function(req, res) {
                          message: 'No documents in collection'
                     });
                } else {
+                    translateBody(body, res);
                     res.render('skills', {
                          title: res.__('Skills'),
                          currenturl: '/skills',
@@ -46,6 +47,13 @@ const skills = function(req, res) {
                }
           }
      );
+}
+
+function translateBody(body, res) {
+     body.forEach(function(part, index, arr) {
+          arr[index].name = res.__(arr[index].name);
+          arr[index].description = res.__(arr[index].description);
+     });
 }
 
 module.exports = {
